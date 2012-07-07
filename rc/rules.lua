22dde5b7e92e1bbf065b0c43791a00d9549dfc28
Vincent Bernat <bernat@luffy.cx>
2012-07-07 20:41:31 +0200

    rules: no window should start maximized

--- rc/rules.lua
+++ rc/rules.lua
@@ -4,6 +4,8 @@ awful.rules.rules = {
      properties = { border_width = beautiful.border_width,
 		    border_color = beautiful.border_normal,
 		    focus = true,
+		    maximized_vertical   = false,
+		    maximized_horizontal = false,
 		    keys = config.keys.client,
 		    buttons = config.mouse.client }},
    { rule = { class = "Emacs" },
