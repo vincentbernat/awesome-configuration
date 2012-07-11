53ca8b7ad5af1114a09eb49beae85c97075d40ca
Vincent Bernat <bernat@luffy.cx>
2012-07-11 09:05:34 +0200

    xlock: also configure DPMS

--- rc/xlock.lua
+++ rc/xlock.lua
@@ -1,9 +1,12 @@
 -- Lockscreen
 
 xrun("xautolock",
-     "xautolock -time 5 -locker 'i3lock -n -i " ..
+     "xautolock -time 3 -locker 'i3lock -n -i " ..
 	awful.util.getdir("cache") .. "/current-wallpaper.png'")
 
 config.keys.global = awful.util.table.join(
    config.keys.global,
    awful.key({}, "XF86ScreenSaver", function() awful.util.spawn("xautolock -locknow", false) end))
+
+-- Configure DPMS
+os.execute("xset dpms 360 720 1200")
