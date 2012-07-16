e3fd83eab45dd013c5d505f8e814321a95504397
Vincent Bernat <bernat@luffy.cx>
2012-07-16 11:30:55 +0200

    xresources: move URxvt configuration into ~/.Xresources

--- rc.lua
+++ rc.lua
@@ -41,40 +41,7 @@ loadrc("errors")		-- errors and debug stuff
 -- Global configuration
 modkey = "Mod4"
 config = {}
-config.terminal = table.concat(
-   { 
-      "urxvtcd",
-      "++iso14755",		-- Disable ISO 14755 mode
-      "+sb",			-- Disable scrollbar
-      "-si -sw",		-- Don't scroll to bottom
-      "-j",			-- Enable jump scrolling
-      "-sl 2000",		-- Scrollback buffer
-      "-bc",			-- Blink cursor
-      "-cr green",		-- Cursor color
-      "-depth 32",
-      -- Color stuff
-      "-bg rgba:0000/0000/0000/eeee",
-      "-fg white",
-      -- See: http://xcolors.net/ ; colorful theme
-      "--color0  rgb:15/15/15",
-      "--color8  rgb:69/69/69",
-      "--color1  rgb:FF/8E/AF",
-      "--color9  rgb:ED/4C/7A",
-      "--color2  rgb:A6/E2/5F",
-      "--color10 rgb:A6/E1/79",
-      "--color3  rgb:F8/E5/78",
-      "--color11 rgb:FF/DF/6B",
-      "--color4  rgb:A6/E2/F0",
-      "--color12 rgb:79/D2/FF",
-      "--color5  rgb:E8/5B/92",
-      "--color13 rgb:BB/5D/79",
-      "--color6  rgb:5F/86/8F",
-      "--color14 rgb:87/A8/AF",
-      "--color7  rgb:D5/F1/F2",
-      "--color15 rgb:E2/F1/F6",
-      "-fn xft:DejaVuSansMono-8",	       -- Font
-      "-letsp -1",			       -- Fix font width
-   }, " ")
+config.terminal = "urxvtcd"
 config.layouts = {
    awful.layout.suit.tile,
    awful.layout.suit.tile.left,
