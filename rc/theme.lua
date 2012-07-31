e975da83f9b3889a8ec2400d31ef0523225ab5c2
Vincent Bernat <bernat@luffy.cx>
2012-07-31 06:59:26 +0200

    theme: don't rely on an external theme
    We mostly redefine the theme. Just redefine everything.

--- rc/theme.lua
+++ rc/theme.lua
@@ -1,39 +1,47 @@
 -- Small modifications to anrxc's zenburn theme
 
 local na = awful.util.color_strip_alpha
-local theme = loadrc("../themes/nice-and-clean-theme/theme")
-if theme then
-   theme.wallpaper_cmd = { "/bin/true" }
-   theme.font = "Terminus 8"
+local icons = awful.util.getdir("config") .. "/icons"
 
-   for n, l in pairs(theme) do
-      local layout = n:match("layout_([%w]+)")
-      if layout then
-	 theme[n] = awful.util.getdir("config") .. "/icons/layouts/" .. layout .. ".png"
-      end
-   end
+theme = {}
+theme.wallpaper_cmd = { "/bin/true" }
+theme.font = "Terminus 8"
+theme.tasklist_font = "DejaVu Sans 8"
 
-   theme.border_width  = 4
-   theme.border_normal = "#00000000"
-   theme.border_focus  = "#FF7F0066"
-   theme.border_marked = theme.border_marked .. "66"
+theme.bg_normal     = "#22222299"
+theme.bg_focus      = "#d8d8d8bb"
+theme.bg_urgent     = "#d02e5499"
+theme.bg_minimize   = "#44444499"
 
-   theme.bg_normal        = theme.bg_normal .. "99"
-   theme.bg_focus         = theme.bg_focus .. "BB"
-   theme.bg_urgent        = theme.bg_urgent .. "99"
-   theme.bg_minimize      = theme.bg_minimize .. "99"
+theme.fg_normal     = "#cccccc"
+theme.fg_focus      = "#000000"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#ffffff"
 
-   -- Widget stuff
-   theme.bg_widget        = "#000000BB"
-   theme.fg_widget_label  = "#737d8c"
-   theme.fg_widget_value  = na(theme.fg_normal)
-   theme.fg_widget_value_important  = "#E80F28"
-   theme.fg_widget_border = theme.fg_widget_label
-   theme.fg_widget_clock  = na(theme.border_focus)
+theme.border_width  = 4
+theme.border_normal = "#00000000"
+theme.border_focus  = "#FF7F0066"
+theme.border_marked = "#91231c66"
 
-   -- Naughty
-   naughty.config.presets.normal.bg = theme.bg_widget
-   naughty.config.default_preset.font = "DejaVu Sans 10"
+-- Widget stuff
+theme.bg_widget        = "#000000BB"
+theme.fg_widget_label  = "#737d8c"
+theme.fg_widget_value  = na(theme.fg_normal)
+theme.fg_widget_value_important  = "#E80F28"
+theme.fg_widget_border = theme.fg_widget_label
+theme.fg_widget_clock  = na(theme.border_focus)
 
-   return theme
+-- Taglist
+theme.taglist_squares_sel   = icons .. "/taglist/squarefw.png"
+theme.taglist_squares_unsel = icons .. "/taglist/squarew.png"
+
+-- Layout icons
+for _, l in pairs(config.layouts) do
+   theme["layout_" .. l.name] = icons .. "/layouts/" .. l.name .. ".png"
 end
+
+-- Naughty
+naughty.config.presets.normal.bg = theme.bg_widget
+naughty.config.default_preset.font = "DejaVu Sans 10"
+
+return theme
