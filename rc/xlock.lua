c30c2062ad3439fd59e2f86c26e7f4bc6348555a
Vincent Bernat <bernat@luffy.cx>
2012-09-15 10:04:34 +0200

    xlock: move xautolock and notify stuff into shell scripts

--- rc/xlock.lua
+++ rc/xlock.lua
@@ -3,13 +3,9 @@
 local icons = loadrc("icons", "vbe/icons")
 
 xrun("xautolock",
-     "xautolock -notify 10 -notifier " ..
-	" 'notify-send Lock\\ screen -i " ..
-	icons.lookup({name = "system-lock-screen", type = "actions" }) ..
-	" -t 10000 " ..
-	"   Lock\\ screen\\ will\\ be\\ started\\ in\\ 10\\ seconds...' " ..
-	" -time 3 -locker " ..
-	" 'i3lock -n -i " .. awful.util.getdir("cache") .. "/current-wallpaper.png'")
+     awful.util.getdir("config") ..
+        "/bin/xautolock " ..
+        icons.lookup({name = "system-lock-screen", type = "actions" }))
 
 config.keys.global = awful.util.table.join(
    config.keys.global,
