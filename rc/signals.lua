b601e48d6a6740f701d63af6bf4e0013622e3ced
Vincent Bernat <bernat@luffy.cx>
2012-09-26 15:05:47 +0200

    signals: raise newly created windows

--- rc/signals.lua
+++ rc/signals.lua
@@ -53,13 +53,14 @@ client.add_signal("manage",
 			   c.icon = image(icon)
 			end
 		     end
-		     
+
 		     if not startup then
 			-- Put windows in a smart way, only if they does not set an initial position.
 			if not c.size_hints.user_position and not c.size_hints.program_position then
 			   awful.placement.no_overlap(c)
 			   awful.placement.no_offscreen(c)
 			end
+                        c:raise()
 		     end
 		  end)
 
