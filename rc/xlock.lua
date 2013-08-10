04b184c6ccf955318396d93a4d578825349feca2
Vincent Bernat <bernat@luffy.cx>
2013-08-10 12:52:37 +0200

    lock: ensure xautolock locks the screen even if disabled

--- rc/xlock.lua
+++ rc/xlock.lua
@@ -9,7 +9,10 @@ xrun("xautolock",
 
 config.keys.global = awful.util.table.join(
    config.keys.global,
-   awful.key({}, "XF86ScreenSaver", function() awful.util.spawn("xautolock -locknow", false) end))
+   awful.key({}, "XF86ScreenSaver",
+             function()
+                awful.util.spawn_with_shell("xautolock -enable ; xautolock -locknow", false)
+             end))
 
 -- Configure DPMS
 os.execute("xset dpms 360 720 1200")
