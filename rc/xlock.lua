f7d82d5c5233d55b0feee48d1c15fc5264eb3f61
Vincent Bernat <vincent@bernat.im>
2014-07-01 16:17:11 +0200

    lock: enable locking with modkey + delete
    Some keyboards are missing a lot of keys...

--- rc/xlock.lua
+++ rc/xlock.lua
@@ -7,13 +7,15 @@ xrun("xautolock",
         "/bin/xautolock " ..
         icons.lookup({name = "system-lock-screen", type = "actions" }))
 
+local lock = function()
+   awful.util.spawn(awful.util.getdir("config") ..
+                    "/bin/locker", false)
+end
+
 config.keys.global = awful.util.table.join(
    config.keys.global,
-   awful.key({}, "XF86ScreenSaver",
-             function()
-                awful.util.spawn(awful.util.getdir("config") ..
-                                 "/bin/locker", false)
-             end))
+   awful.key({}, "XF86ScreenSaver", lock),
+   awful.key({ modkey, }, "Delete", lock))
 
 -- Configure DPMS
 os.execute("xset dpms 360 720 1200")
