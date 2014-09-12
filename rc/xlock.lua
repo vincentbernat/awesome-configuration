61dfa49fb7076876d6e2095f30189781c756c810
Vincent Bernat <vincent@bernat.im>
2014-09-12 15:01:52 +0200

    lock: start xss-lock in xsession
    It seems that spawning it will just crash awesome.

--- rc/xlock.lua
+++ rc/xlock.lua
@@ -8,7 +8,3 @@ config.keys.global = awful.util.table.join(
    config.keys.global,
    awful.key({}, "XF86ScreenSaver", lock),
    awful.key({ modkey, }, "Delete", lock))
-
--- Configure xss-lock
-os.execute(awful.util.spawn(awful.util.getdir("config") ..
-                            "/bin/xss-lock start"))
