c1c6ca9324ad75909ff790e896975668aa9e816f
Vincent Bernat <vincent@bernat.im>
2014-09-19 15:54:57 +0200

    compton: don't use glx backend
    It doesn't work well with newer Intel drivers.

--- rc/start.lua
+++ rc/start.lua
@@ -1,5 +1,5 @@
 -- Spawn a composoting manager
-awful.util.spawn("compton --vsync opengl-swc --backend glx --glx-no-rebind-pixmap", false)
+awful.util.spawn("compton --vsync drm", false)
 
 -- Start idempotent commands
 local execute = {
