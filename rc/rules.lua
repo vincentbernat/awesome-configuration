474722362573e24bebaf8efe02d6d9861f3ff96f
Vincent Bernat <vincent@bernat.im>
2014-09-25 20:38:39 +0200

    terminal: use an icon for terminal
    `vbeterm` doesn't come with an icon.

--- rc/rules.lua
+++ rc/rules.lua
@@ -21,6 +21,9 @@ awful.rules.rules = {
 	   end
 	end
      end },
+   { rule = { class = config.termclass },
+     properties = { icon = image(icons.lookup({ name = "gnome-terminal",
+                                                type = "apps" })) } },
    { rule_any = { class = { "Iceweasel", "Firefox", "Chromium", "Conkeror", "Google-chrome" } },
      callback = function(c)
 	-- All windows should be slaves, except the browser windows.
