e3747d1b9f8451d287c38bb5b594d1d9b2986a7a
Vincent Bernat <vincent@bernat.im>
2015-07-12 11:31:13 +0200

    spotify: try to support the new version of Spotify
    Use dbus-mediakeys to get an appropriate DBus interface. In future
    versions, this should not be needed anymore. See:
     https://github.com/vincentbernat/dbus-mediakeys

--- rc/start.lua
+++ rc/start.lua
@@ -17,7 +17,9 @@ local execute = {
       "text/html",
    -- Default MIME types
    "xdg-mime default evince.desktop application/pdf",
-   "xdg-mime default gpicview.desktop image/png image/x-apple-ios-png image/jpeg image/jpg image/gif"
+   "xdg-mime default gpicview.desktop image/png image/x-apple-ios-png image/jpeg image/jpg image/gif",
+   -- dbus-mediakeys
+   "dbus-mediakeys &"
 }
 
 os.execute(table.concat(execute, ";"))
