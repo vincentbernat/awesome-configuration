1342c2b227745f4ca501247a2e7379c67b89adae
Vincent Bernat <vincent@bernat.im>
2016-04-29 16:38:56 +0200

    spotify: do not expect an answer to DBus request
    Using os.execute makes awesome block during execution. Don't wait for
    anything.

--- lib/spotify.lua
+++ lib/spotify.lua
@@ -32,7 +32,7 @@ end
 local function cmd(command)
    local client = spotify()
    if client then
-      os.execute("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify " ..
+      os.execute("dbus-send --dest=org.mpris.MediaPlayer2.spotify " ..
          "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player." .. command)
    end
 end
