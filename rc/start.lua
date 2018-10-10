c8ada41ed4eacbb8290fadc4e8e562d74c788cc7
Vincent Bernat <vincent@bernat.ch>
2018-10-10 13:19:43 +0200

    mime: update MIME handler for PDF
    Newest applications are using vendor-name.desktop scheme.

--- rc/start.lua
+++ rc/start.lua
@@ -12,7 +12,7 @@ local execute = {
       "x-scheme-handler/https " ..
       "text/html",
    -- Default MIME types
-   "xdg-mime default evince.desktop application/pdf",
+   "xdg-mime default org.gnome.Evince.desktop application/pdf",
    "xdg-mime default gpicview.desktop image/png image/x-apple-ios-png image/jpeg image/jpg image/gif"
 }
 
